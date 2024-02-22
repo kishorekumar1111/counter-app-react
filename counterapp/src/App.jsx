9be71c0aa80913a841eb37de04ebf39561382508
kishorekumar1111 <kishore.kumar@kalvium.community>
2024-02-22 13:42:26 +0530

    donr

--- counterapp/src/App.jsx
+++ counterapp/src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
+import React, { Component } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export class App extends Component {
+
+  constructor(){
+    super()
+    this.state = {
+      count:0
+    }
+  }
+ 
+incrementCount=()=>{
+  this.setState((b_state)=>({
+    count:b_state.count+1
+  }))
+}
+
+decrementCount=()=>{
+  this.setState((b_state)=>{
+    return{
+    count:b_state.count-1
+    }
+})
+}
+
+
+reset=()=>{
+  this.setState(()=>({count:0}))
+}
+
+  render(){
+    return (
+      <>
+        <center>
+          <h1>Counter App</h1>
+          <h2>{this.state.count}</h2>
+          <span>
+            <button onClick={this.incrementCount}>+</button>
+            <button onClick={this.decrementCount}>-</button>
+            <button onClick={this.reset}>Reset</button>
+          </span>
+        </center>
+      </>
+    )
+  }
 }
 
 export default App
